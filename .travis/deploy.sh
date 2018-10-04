c52afa89f5578ac13c6a567fa604e3d5d405dc68
Jake Turner <jaketurner25@live.com>
2018-10-04 11:21:56 +0100

    Extension publishing
    Signed-off-by: Jake Turner <jaketurner25@live.com>

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,6 +17,10 @@
 set -ev
 set -o pipefail
 
+if [ "${TASK}" != "systest" ]; then
+    exit 0
+fi
+
 # Grab the current root directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 cd ${DIR}/client
@@ -28,7 +32,7 @@ if [ "${TRAVIS_NODE_VERSION}" != "" -a "${TRAVIS_NODE_VERSION}" != "8" ]; then
 fi
 
 # Check that this is the main repository.
-if [[ "${TRAVIS_REPO_SLUG}" != hyperledger* ]]; then
+if [[ "${TRAVIS_REPO_SLUG}" != Jakeeyturner* ]]; then
   echo "Skipping deploy; wrong repository slug."
   exit -1
 fi
@@ -36,8 +40,43 @@ fi
 # Push the code to npm there there is a travis tag defined
 if [ "${TRAVIS_TAG}" != "" ]; then
 
+
   # publish to the VSCode marketplace using VSCETOKEN
-  vsce publish -p ${VSCETOKEN}
+#   echo "THIS IS WHERE VSCE PUBLISH -P TOKEN WOULD OCCUR"
+#   insert shit here
+#   exit -1
+
+  # We will need the node modules installed before publishing
+  #   npm install
+  #   vsce publish -p ${VSCETOKEN}
+
+  # Configure the Git repository and clean any untracked and unignored build files.
+  npm install -g @alrra/travis-scripts
+  set-up-ssh --key "$encrypted_43285c67a7b0_key" \
+                             --iv "$encrypted_43285c67a7b0_iv" \
+                             --path-encrypted-key "../.travis/github_deploy_key.enc"
+
+  git config user.name "${GH_USER_NAME}"
+  git config user.email "${GH_USER_EMAIL}"
+  git checkout -b issue-16
+  git reset --hard
+  git clean -d -f
+
+  npm install semver
+
+  # Bump the version number.
+  node ./scripts/pkgbump.js
+
+  export NEW_VERSION=$(node -e "console.log(require('./package.json').version)")
+
+# Change from HTTPS to SSH.
+  ../.travis/fix_github_https_repo.sh
+
+  # Add the version number changes and push them to Git.
+  git add .
+  git commit -m "Automatic version bump to ${NEW_VERSION}"
+  git push origin issue-16
+
 fi
 
 echo Successfully published the new version
